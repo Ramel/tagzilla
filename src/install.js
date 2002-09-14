f5dde85bf8de4a5861d4f7c470c40634c43bd840
perry <perry>
2002-09-14 18:26:48 +0000

    Box it up and call it 0.025

--- src/install.js
+++ src/install.js
@@ -1,7 +1,7 @@
 // customizable behavior
 var shortName = "tagzilla";
-var longName = "TagZilla 0.014";
-var gVersion = "0.014";
+var longName = "TagZilla 0.025";
+var gVersion = "0.025";
 var srDest = 100;
 
 // this function verifies disk space in kilobytes
